1169c6eef19f35f14b19db2f223d9b023f532ef6
Bartosz Jędrzejewski <bjedrzejewsk@gmail.com>
2024-03-11 22:34:42 +0100

    feat: :twisted_rightwards_arrows: Add playlist random track playback

--- handler/playback.go
+++ handler/playback.go
@@ -3,10 +3,31 @@ package handler
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 
 	"github.com/zmb3/spotify"
 )
+
+func RandomTrackPlayback(w http.ResponseWriter, r *http.Request) {
+	log.Print("Playback toggled:")
+	state, err := Client.PlayerState()
+	if err != nil {
+		log.Println(err)
+	}
+	if state.CurrentlyPlaying.Playing {
+		TogglePlaybackState(state)
+	}
+	if breakIfNoActiveDevices() {
+		return
+	}
+
+	uri := "spotify:playlist:" + spotify.URI(playlist.Url)
+	offset := spotify.PlaybackOffset{Position: rand.Intn(len(playlist.Tracks))}
+	playbackOptions := spotify.PlayOptions{PlaybackContext: &uri, PlaybackOffset: &offset}
+	Client.PlayOpt(&playbackOptions)
+
+}
 
 func StartStopPlayback(w http.ResponseWriter, r *http.Request) {
 	log.Print("Playback toggled:")
@@ -15,15 +36,23 @@ func StartStopPlayback(w http.ResponseWriter, r *http.Request) {
 		log.Fatalln(err)
 	}
 
+	if breakIfNoActiveDevices() {
+		return
+	}
+
+	TogglePlaybackState(state)
+}
+
+func breakIfNoActiveDevices() bool {
 	devices, err := Client.PlayerDevices()
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err)
 	}
-	if len(devices) != 0 {
-		TogglePlaybackState(state)
-	} else {
+	if len(devices) == 0 {
 		fmt.Println("No active user's devices found")
+		return true
 	}
+	return false
 }
 
 func TogglePlaybackState(state *spotify.PlayerState) {
